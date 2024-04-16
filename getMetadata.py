7a40149d1c9b550ea18a50bc3ccbebb8414f027c
ShaneBerhoff <57015490+ShaneBerhoff@users.noreply.github.com>
2024-04-15 20:00:56 -0400

    metadata for Savee

--- getMetadata.py
+++ getMetadata.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from pathlib import Path
+import re
 
 def metadataCrema(directoryPath):
     dataPath = Path(directoryPath)
@@ -18,20 +19,39 @@ def metadataCrema(directoryPath):
         filename = filePath.name
         label = filename.split('_')[2]
         labelID = idDict[label]
-        data.append((filename, label, labelID))
+        data.append((filename, labelID))
         
-    df = pd.DataFrame(data, columns=['Filename', 'Label', 'ID'])
+    df = pd.DataFrame(data, columns=['filename', 'label'])
     
     return df
 
 def metadataRavdess():
     return
 
-def metadataSavee():
-    return
+def metadataSavee(directoryPath):
+    dataPath = Path(directoryPath)
+    audioFiles = dataPath.glob('*.wav')
+    
+    idDict = {
+        "sa": 0,
+        "a": 1,
+        "d": 2,
+        "f": 3,
+        "h": 4,
+        "n": 5
+    }
+    
+    data = []
+    for filePath in audioFiles:
+        filename = filePath.name
+        label = re.search(r"_(\D+)\d", filename).group(1)
+        labelID = idDict.get(label)
+        if labelID is not None:
+            data.append((filename, labelID))
+        
+    df = pd.DataFrame(data, columns=['filename', 'label'])
+    
+    return df
 
 def metadataTess():
     return
-
-df = metadataCrema('Data/archive/Crema')
-df.to_csv('Data/CremaMetadata.csv', index=False)
