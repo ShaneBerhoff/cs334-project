715cf3ac002b0e190b005184c591320d3bbca295
ShaneBerhoff <57015490+ShaneBerhoff@users.noreply.github.com>
2024-04-20 16:44:22 -0400

    improve dataloader

--- src/data_loader.py
+++ src/data_loader.py
@@ -9,13 +9,13 @@ import util
 # Sound Dataset
 # ----------------------------
 class SoundDS(Dataset):
-    def __init__(self, df, data_path):
+    def __init__(self, df, data_path, duration=2618, sr=16000, channel=2, shift_pct=0.3):
         self.df = df
         self.data_path = str(data_path)
-        self.duration = 2618
-        self.sr = 16000
-        self.channel = 2
-        self.shift_pct = 0.3
+        self.duration = duration
+        self.sr = sr
+        self.channel = channel
+        self.shift_pct = shift_pct
                         
     # ----------------------------
     # Number of items in dataset
@@ -33,6 +33,7 @@ class SoundDS(Dataset):
         # Get the Class ID
         class_id = self.df.loc[idx, 'label']
 
+        # Load audio file
         aud = AudioUtil.load_audio(audio_file)
         # Some sounds have a higher sample rate, or fewer channels compared to the
         # majority. So make all sounds have the same number of channels and same 
@@ -41,34 +42,37 @@ class SoundDS(Dataset):
         # the same.
         reaud = AudioUtil.resample(aud, self.sr)
         rechan = AudioUtil.rechannel(reaud, self.channel)
-
         dur_aud = AudioUtil.pad_trunc(rechan, self.duration)
+        
+        # Shift audio
         shift_aud = AudioUtil.time_shift(dur_aud, self.shift_pct)
+        # Create spectrogram
         sgram = AudioUtil.spectro_gram(shift_aud, n_mels=64, n_fft=1024, hop_len=None)
+        # Augment spectrogram
         aug_sgram = AudioUtil.spectro_augment(sgram, max_mask_pct=0.1, n_freq_masks=2, n_time_masks=2)
 
         return aug_sgram, class_id
 
 
-def get_loaders():
+def get_loaders(batch_size=16, split_ratio=0.8, num_workers=0):
     data_path = util.from_base_path("/")
     df = metadata.Metadata(util.from_base_path("/Data/archive/")).getMetadata()
     myds = SoundDS(df, data_path)
 
     # Random split of 80:20 between training and validation
     num_items = len(myds)
-    num_train = round(num_items * 0.8)
+    num_train = round(num_items * split_ratio)
     num_val = num_items - num_train
     train_ds, val_ds = random_split(myds, [num_train, num_val])
 
     # Create training and validation data loaders
-    train_dl = DataLoader(train_ds, batch_size=16, shuffle=True)
-    val_dl = DataLoader(val_ds, batch_size=16, shuffle=False)
+    train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    val_dl = DataLoader(val_ds, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
     return train_dl, val_dl
 
 
-def get_subsample_loaders():
+def get_subsample_loaders(batch_size=16, split_ratio=0.8, num_workers=0):
     data_path = util.from_base_path("/")
     df = metadata.Metadata(util.from_base_path("/Data/archive/")).getMetadata()
 
@@ -80,17 +84,17 @@ def get_subsample_loaders():
 
     # Random split of 80:20 between training and validation
     num_items = len(subds)
-    num_train = round(num_items * 0.8)
+    num_train = round(num_items * split_ratio)
     num_val = num_items - num_train
     train_ds, val_ds = random_split(subds, [num_train, num_val])
 
     # Create training and validation data loaders
-    train_dl = DataLoader(train_ds, batch_size=16, shuffle=True)
-    val_dl = DataLoader(val_ds, batch_size=16, shuffle=False)
+    train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    val_dl = DataLoader(val_ds, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
     return train_dl, val_dl
 
-
+# Check for working
 if __name__ == '__main__':
     data_path = "/"
     df = metadata.Metadata(util.from_base_path("/Data/archive/")).getMetadata()
