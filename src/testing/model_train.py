674417d3ef2d9360e3acb3d9130b160f49f5d586
ShaneBerhoff <57015490+ShaneBerhoff@users.noreply.github.com>
2024-04-21 15:35:03 -0400

    main loop

--- src/testing/model_train.py
+++ src/testing/model_train.py
@@ -9,24 +9,28 @@ import torch
 from data_loader import get_loaders
 import time
 
-# set up model arch
-model = TuningAudioClassifier()
+def main():
+    # set up model arch
+    model = TuningAudioClassifier()
 
-# put on gpu if available
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model = model.to(device)
+    # put on gpu if available
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
 
-start = time.time()
-# set up dataloader
-trainDL, testDL = get_loaders()
-print(f"Data loader time: {time.time() - start}")
+    start = time.time()
+    # set up dataloader
+    trainDL, testDL = get_loaders()
+    print(f"Data loader time: {time.time() - start}")
 
-# train model
-start = time.time()
-training(model, trainDL, 1, device)
-print(f"Train time: {time.time() - start}")
+    # train model
+    start = time.time()
+    training(model, trainDL, 1, device)
+    print(f"Train time: {time.time() - start}")
 
-# predictions
-start = time.time()
-predict(model, testDL, device)
-print(f"Predict time: {time.time() - start}")
+    # predictions
+    start = time.time()
+    predict(model, testDL, device)
+    print(f"Predict time: {time.time() - start}")
+    
+if __name__ == '__main__':
+    main()
