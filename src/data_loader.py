3193491c8854e094d99554bb9772fc516fecea64
andrewtlu <andrewlulu2012@gmail.com>
2024-04-23 01:45:39 -0400

    Add transforms to data_loader

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,12 +4,22 @@ import os
 import metadata
 import util
 import torch
+import torchvision.transforms as transforms
+
+
+TRANSFORMS = {
+    'mobilenetv3': transforms.Compose([
+        transforms.Lambda(lambda x: x.expand(3, -1, -1)),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ]),
+}
+
 
 # ----------------------------
 # Sound Dataset
 # ----------------------------
 class SoundDS(Dataset):
-    def __init__(self, data_path, shift_pct, n_mels, n_fft, hop_len, max_mask_pct, n_masks):
+    def __init__(self, data_path, shift_pct, n_mels, n_fft, hop_len, max_mask_pct, n_masks, model=None):
         self.data_path = str(data_path)
         self.shift_pct = shift_pct
         self.n_mels = n_mels
@@ -18,6 +28,7 @@ class SoundDS(Dataset):
         self.max_mask_pct = max_mask_pct
         self.n_masks = n_masks
         self.len = len([name for name in os.listdir(data_path) if os.path.isfile(os.path.join(data_path, name))])
+        self.transform = TRANSFORMS[model] if model else None
                         
     # ----------------------------
     # Number of items in dataset
@@ -47,6 +58,11 @@ class SoundDS(Dataset):
         # Augment spectrogram
         aug_sgram = AudioUtil.spectro_augment(sgram, self.max_mask_pct, n_freq_masks=self.n_masks, n_time_masks=self.n_masks)
 
+        # Transform for model
+        if self.transform:
+            aug_sgram = self.transform(aug_sgram)
+
+        print(aug_sgram.shape)
         return aug_sgram, classID
 
 
@@ -65,6 +81,13 @@ def get_loaders(batch_size=32, split_ratio=0.8, num_workers=4, shift_pct=0.3, n_
     val_dl = DataLoader(val_ds, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True)
 
     return train_dl, val_dl
+
+
+def get_test_loader(n_mels, n_fft, hop_len):
+    data_path = util.from_base_path("/Data/tensors/")
+    myds = SoundDS(data_path, shift_pct=0.3, n_mels=n_mels, n_fft=n_fft, hop_len=hop_len, max_mask_pct=0.1, n_masks=2, model='mobilenetv3')
+    return DataLoader(myds, batch_size=1, shuffle=False)
+
 
 # Check for working
 if __name__ == '__main__':
