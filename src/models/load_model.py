a174ce41fbfc4f3e8077eaee8a9b269451ad0961
andrewtlu <andrewlulu2012@gmail.com>
2024-04-24 13:10:32 -0400

    Create load_best_model

--- src/models/load_model.py
+++ src/models/load_model.py
@@ -6,6 +6,49 @@ import time
 import os
 import re
 
+def load_best_model(model_info, workers=6, options={}):
+    """
+    Load the best model from the weights directory alongside the correct test/train indices.
+
+    Args:
+        model: The specific model in pipeline to load.
+        model_path: The path to the model directory, starting from cs334-project/Data/models.
+    """
+    base_path = os.path.join(util.from_base_path("/Data/models"))
+    model_path = os.path.join(base_path, model_info["path"])
+    weights_dir = os.path.join(model_path, "Weights")
+
+    if not os.path.exists(weights_dir):
+        raise FileNotFoundError(f"The directory {weights_dir} does not exist.")
+    
+    files = [f for f in os.listdir(weights_dir) if f.endswith('.pt')]
+    max_number = -1
+    latest_file = None
+    number_regex = re.compile(r'(\d+)\.pt$')
+    
+    for file in files:
+        # Search for numbers in the filename
+        match = number_regex.search(file)
+        if match:
+            # Convert the found number to an integer
+            number = int(match.group(1))
+            # Update the latest file if this file has a higher number
+            if number > max_number:
+                max_number = number
+                latest_file = file
+
+    if latest_file is None:
+        raise FileNotFoundError("No suitable .pt file found in the Weights directory.")
+
+    print(f"Loading model with saved weights: {latest_file}")
+
+    m = model_info["model"](input_path=os.path.join(weights_dir, latest_file), **options) # double star to unpack dictionary
+    train_dl, test_dl = get_existing_loader(model_path=model_path, batch_size=model_info["batch"], num_workers=workers, n_mels=model_info["n_mels"], n_fft=2048, hop_len=model_info["hop_len"], transform=m.transform)
+    
+    
+    return m, train_dl, test_dl
+
+
 def main(full, batch, workers, model_path, load_epoch, Train, max_epochs):
     # Fines the latest epoch file in weights and uses it
     weights_dir = os.path.join(model_path, "Weights")
