4611e3d953a228850b4a0bf37c0e694ab4160de3
ShaneBerhoff <57015490+ShaneBerhoff@users.noreply.github.com>
2024-04-24 12:21:03 -0400

    look at the spgrams being put into the models

---
+++ src/models/spectrogram_check.py
@@ -0,0 +1,35 @@
+from pipeline import models
+from data_loader import get_loaders
+import matplotlib.pyplot as plt
+
+def main(T):
+    for m in models:
+        model_info = models[m]
+        model = model_info["model"]()
+        train_dl, _, _, _ = get_loaders(batch_size=1, num_workers=6, n_mels=model_info["n_mels"], n_fft=2048, hop_len=model_info["hop_len"], transform={model.transform if T else None})
+        
+        for sgram, _ in train_dl:
+            print(f"Item in {model.name()} of shape: {sgram.shape}")
+            # Check if sgram is a single or multi-channel image and adjust accordingly
+            if sgram.shape[1] == 1:  # Single channel
+                image_data = sgram[0].numpy().squeeze()
+                cmap = 'viridis'  # Suitable for single channel data
+            # Adjust the tensor for display
+            elif sgram.shape[1] == 3:  # Confirming that it's an RGB image
+                image_data = sgram[0].permute(1, 2, 0).numpy()  # Remove batch dimension and permute to [H, W, C]
+                cmap = None
+
+            # Setting up the plot
+            fig, axs = plt.subplots(1, 1, figsize=(10, 4))
+            im = axs.imshow(image_data, origin='lower', aspect='auto', cmap=cmap)
+            axs.set_title('Mel Spectrogram (dB)')
+            axs.set_ylabel('Mel Frequency Bin')
+            axs.set_xlabel('Time Window')
+            plt.colorbar(im, ax=axs)
+            
+            plt.savefig(f'Data/Spectrogram/{model.name()}.png')
+            plt.close()
+            break
+        
+if __name__ == '__main__':
+    main(False)
